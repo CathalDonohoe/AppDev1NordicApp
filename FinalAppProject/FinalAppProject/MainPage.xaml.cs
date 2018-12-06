98cd29f1d71dc9378fa20e36ce3c298ca57da63b
cathal donohoe <cdonohoe1374@gmail.com>
2018-12-06 12:26:45 +0000

    Importing project

---
+++ FinalAppProject/FinalAppProject/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FinalAppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            bool isUserEmpty, isPwdEmpty;
+            isUserEmpty = String.IsNullOrEmpty(entUser.Text);
+            isPwdEmpty = String.IsNullOrEmpty(entPassword.Text);
+
+            if(isUserEmpty || isPwdEmpty)
+            {
+
+            }
+
+            else
+            {
+                Navigation.PushAsync(new NordicGodsPage());
+            }
+        }
+    }
+}
